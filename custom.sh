54a8cbcb2d87f485741985387c20d52d40082bf3
asifthat <131216364+asifthat@users.noreply.github.com>
2024-06-17 22:57:16 -0400

    Create custom.sh

---
+++ custom.sh
@@ -0,0 +1,33 @@
+cd /home
+
+export SNAPSHOT_PATH=$PWD/chain-configuration/ignition
+
+export PATH="${HOME}/.fuelup/bin:${PATH}"
+
+export ETHEREUM_RPC_ENDPOINT=$(cat  $PWD/apiurl.txt)
+
+if [ -f "$PWD/key.json" ]; then 
+    export P2P_PRIVATE_KEY=$(cat "$PWD/key.json" | jq -r '.secret')
+else 
+    export P2P_PRIVATE_KEY=$($HOME/.fuelup/bin/fuel-core-keygen new --key-type peering > "$PWD/key.json" && cat "$PWD/key.json" | jq -r '.secret')
+fi
+
+${HOME}/.fuelup/bin/fuel-core run \
+--service-name=fuel-sepolia-testnet-node \
+--keypair $P2P_PRIVATE_KEY \
+--relayer $ETHEREUM_RPC_ENDPOINT \
+--ip=0.0.0.0 --port=4000 --peering-port=30333 \
+--db-path=~/.fuel-sepolia-testnet \
+--snapshot $SNAPSHOT_PATH \
+--utxo-validation --poa-instant false --enable-p2p \
+--reserved-nodes /dns4/p2p-testnet.fuel.network/tcp/30333/p2p/16Uiu2HAmDxoChB7AheKNvCVpD4PHJwuDGn8rifMBEHmEynGHvHrf \
+--sync-header-batch-size 100 \
+--enable-relayer \
+--relayer-v2-listening-contracts=0x01855B78C1f8868DE70e84507ec735983bf262dA \
+--relayer-da-deploy-height=5827607 \
+--relayer-log-page-size=500 \
+--sync-block-stream-buffer-size 30
+
+curl -X POST http://0.0.0.0:4000/v1/graphql \
+  -H "Content-Type: application/json" \
+  -d '{"query": "{ nodeInfo{nodeVersion}}"}' > queryresult.txt
